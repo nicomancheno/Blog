fcbd7cac7b158694c5fc53f9611cb74de0565432
Nicolas Mancheno <nicolasmancheno21@hotmail.com>
2021-10-06 12:52:52 +0200

    Avance 1

--- Nico_Blog/Blog/views.py
+++ Nico_Blog/Blog/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, "Home.html")
+    return render(request, "layout.html")
--- Nico_Blog/Nico_Blog/urls.py
+++ Nico_Blog/Nico_Blog/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('Blog/',include("Blog.urls"))
+    path("",include("Blog.urls"))
 ]
