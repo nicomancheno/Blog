591bf9ebc1aceb8b3a00697174dbf091dcfb5f7a
Nicolas Mancheno <nicolasmancheno21@hotmail.com>
2021-10-08 12:29:08 +0200

    Heroku first

--- Nico_Blog/Blog/forms.py
+++ Nico_Blog/Blog/forms.py
@@ -1,12 +1,15 @@
 from django.forms import ModelForm
 from django import forms
+from django.utils.translation import gettext_lazy as _
+from django.forms.widgets import Textarea
 from .models import *
 
 class CommentForm(ModelForm):
     class Meta:
         model = Comment
         fields = ['name', 'body']
+        labels = {'body':'Comment'}
         widgets = {
-            'name': forms.TextInput(attrs={ 'class':"form-control", 'id':"lastName"}),
-            'body': forms.TextInput(attrs={ 'class':"form-control", 'id':"address"})
+            'name': forms.TextInput(attrs={ 'class':"name-control", 'id':"name"}),
+            'body': Textarea(attrs={ 'class':"body-control", 'id':"body"})
             }
